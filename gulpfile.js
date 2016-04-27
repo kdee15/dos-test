c86ea1d07cea162d6d9bd245e300e4aedfc818b0
Kirk Daniels <kdee15@gmail.com>
2016-04-27 21:29:24 +0200

    js added

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
+// A. REGISTER COMPONENTS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 var gulp = require("gulp"),
     connect = require("gulp-connect"),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload'),
-    autoprefixer = require('gulp-autoprefixer')
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    jscs = require('gulp-jscs')
+    
 ;
 
-// A. CSS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// A. END +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// B. CSS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 gulp.task('compile-sass', function () {
 
@@ -15,10 +27,30 @@ gulp.task('compile-sass', function () {
 
 });
 
-// A. END +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// B. END +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// C. JAVASCRIPT ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('public/ignored_stuff/js/scripts/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('public/ignored_stuff/js/scripts/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('public/scripts'))
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/scripts'));
+});
+
+// C. END +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// B. END +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 gulp.task("start", function () {
 
@@ -36,7 +68,15 @@ gulp.task("start", function () {
         });
     
         livereload.listen();
+    
+        gulp.watch(['public/*.html',
+                    'public/*/*.html',
+                    'public/**/*/*.html',
+                    'public/styles/stylesheet.css',
+                    'public/scripts/js/app.js']);
+    
         gulp.watch('public/ignored_stuff/**/*.scss', ['compile-sass']);
+        gulp.watch('public/ignored_stuff/js/scripts/*', ['lint', 'scripts']);
         
 
 });
